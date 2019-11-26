f8398d4670300b1dc9dff92fc01fc967fd340b26
sbibik <sbibik@waveplatform.com>
2019-11-26 19:47:34 +0300

    step

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const vuepressConfig = require('./.vuepress/config');
 // const vuepressEnhanceApp = require('./.vuepress/enhanceApp');
 const server = require('./server');
 module.exports = {
-  vuepressConfig,
-  // vuepressEnhanceApp,
-  server,
+  getVuepressConfig,
+  // runVuepressEnhanceApp,
+  runServer,
 };
