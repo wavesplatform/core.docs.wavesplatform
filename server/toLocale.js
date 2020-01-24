e7a7a7087b99300cfd1a9ef2305a5496dab49505
sbibik <sbibik@waveplatform.com>
2020-01-24 18:49:13 +0300

    step fixes from task-list

--- server/toLocale.js
+++ server/toLocale.js
@@ -1,9 +1,15 @@
 const ruLangKey = 'ru-RU';
+const defaultLocale = 'en-US';
+
 module.exports = () => {
     return async (ctx, next) => {
         if (ctx.req.url === '/') {
             const acceptLanguage = ctx.header['accept-language'];
             const cookieLang = ctx.cookie && ctx.cookie.lang;
+            if(defaultLocale && !cookieLang) {
+                ctx.redirect('/en/');
+                return;
+            }
             if(
               cookieLang === ruLangKey ||
               acceptLanguage.includes(ruLangKey) && !cookieLang
