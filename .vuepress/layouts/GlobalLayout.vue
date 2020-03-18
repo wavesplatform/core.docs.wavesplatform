34d83af540c2c80bffd1b68ab542d340918392a3
sbibik <sbibik@waveplatform.com>
2020-03-18 13:38:31 +0300

    router back step fix

--- .vuepress/layouts/GlobalLayout.vue
+++ .vuepress/layouts/GlobalLayout.vue
@@ -1,8 +1,8 @@
 <template>
     <component
-        :is="layout"
-        :class="$style.root"
-        :style="{
+            :is="layout"
+            :class="$style.root"
+            :style="{
             opacity: isShowRoot ? 1 : 0,
         }"
     />
@@ -16,9 +16,9 @@
             OnlyContent,
         },
         data() {
-          return {
-              isShowRoot: false,
-          }
+            return {
+                isShowRoot: false,
+            }
         },
         computed: {
             isUserNaturalScrollState() {
@@ -38,7 +38,7 @@
                 return 'NotFound';
             },
             isThemePainted() {
-              return this.$store.state.interface.isThemePainted;
+                return this.$store.state.interface.isThemePainted;
             },
         },
 
@@ -63,35 +63,41 @@
 
                 document.addEventListener('click', (event) => {
                     const target = event.target;
-                   if(target.tagName !== 'A') {
-                       return;
-                   }
-                   if(target.href.replace(target.origin, '') === this.$route.fullPath) {
-                       return;
-                   }
-
-                   if(new RegExp(location.host).test(target.href) && !('__vue__' in target)) {
-                       event.preventDefault();
-                       this.$router.replace(target.pathname + target.search + target.hash)
-                       .catch(error => {
-                           console.error(error)
-                           if(error) {
-                               this.$router.push(this.$localePath + '404');
-                           }
-                       });
-                       return;
-                   }
-                   const targetHostname = target.hostname;
-                   const isExternalUrl = targetHostname !== location.hostname;
-                   let targetHash = decodeURI(target.hash);
-                   const isCyrillicURI = new RegExp('[А-я]').test(targetHash);
-                   if(isCyrillicURI) {
-                       targetHash = targetHash.replace(/л/gi, 'n');
-                   }
-                   if(!isExternalUrl && targetHash) {
-                       event.preventDefault();
-                       scrollToHashElement(targetHash, this.$store);
-                   }
+                    if(target.tagName !== 'A') {
+                        return;
+                    }
+                    if(target.href.replace(target.origin, '') === this.$route.fullPath) {
+                        return;
+                    }
+                    if(new RegExp(location.host).test(target.href) && !('__vue__' in target)) {
+                        event.preventDefault();
+                        // target.href.replace(new RegExp(`${target.hash}$`), '')
+                        const url = target.pathname + target.search + target.hash;
+                        let routerFollowing = null;
+                        if(target.pathname + target.search === location.pathname + location.search) {
+                            routerFollowing = this.$router.replace(url);
+                        } else {
+                            routerFollowing = this.$router.push(url);
+                        }
+                        routerFollowing.catch(error => {
+                            console.error(error)
+                            if(error) {
+                                this.$router.push(this.$localePath + '404');
+                            }
+                        });
+                        return;
+                    }
+                    const targetHostname = target.hostname;
+                    const isExternalUrl = targetHostname !== location.hostname;
+                    let targetHash = decodeURI(target.hash);
+                    const isCyrillicURI = new RegExp('[А-я]').test(targetHash);
+                    if(isCyrillicURI) {
+                        targetHash = targetHash.replace(/л/gi, 'n');
+                    }
+                    if(!isExternalUrl && targetHash) {
+                        event.preventDefault();
+                        scrollToHashElement(targetHash, this.$store);
+                    }
                 });
 
                 this.$watch('$localeConfig', (newValue) => {
